b28de7aa96fc738e4bb25440081aefee2c5fdc05
OliaSednev <oliakas999@gmail.com>
2016-07-05 12:25:10 +0300

    .

--- EX8_2_Olia_Sednev/MailSystem/Program.cs
+++ EX8_2_Olia_Sednev/MailSystem/Program.cs
@@ -10,24 +10,6 @@ namespace MailSystem
 {
     internal class Program
     {
-        private class TimerState
-        {
-            public int Counter = 0;
-            public Timer Timer;
-        }
-
-        private static void CheckStatus(object state)
-        {
-            var mailData = new MailManager();
-            mailData.MailArrived += (obj, mailevenArgs) => { Console.WriteLine(mailevenArgs.Title); };
-            mailData.MailArrived += (obj, mailevenArgs) => { Console.WriteLine(mailevenArgs.Body); };
-            mailData.SimulateMailArrived();
-
-            var timerState = (TimerState)state;
-            timerState.Counter++;
-            Console.WriteLine($"{DateTime.Now.TimeOfDay} Checking Status {timerState.Counter}.");
-
-        }
 
         public static void Main(string[] args)
         {
@@ -37,24 +19,59 @@ namespace MailSystem
             mailData.MailArrived += (obj, mailevenArgs) => { Console.WriteLine(mailevenArgs.Body); };
 
             mailData.SimulateMailArrived();
+            var timer = new Timer(TimerCallback, null, 1000, 1000);
+            Console.ReadLine();
 
+        }
 
-            var timerState = new TimerState();
+        private static void TimerCallback(object state)
+        {
+            var mailData = new MailManager();//publisher
 
-            // Delegate that invokes methods for the timer.
-            var timerDelegate = new TimerCallback(CheckStatus);
+            mailData.MailArrived += (obj, mailevenArgs) => { Console.WriteLine(mailevenArgs.Title); };
+            mailData.MailArrived += (obj, mailevenArgs) => { Console.WriteLine(mailevenArgs.Body); };
 
-            // timer that waits one second, then invokes every second.
-            var timer = new Timer(timerDelegate, timerState, 1000, 1000);
-
-            // Keep a handle to the timer, so it can be disposed.
-            timerState.Timer = timer;
-
-            // The main thread does nothing until the timer is disposed.
-            while (timerState.Timer != null)
-            {
-                Thread.Sleep(0);
-            }
+            mailData.SimulateMailArrived();
+            Console.WriteLine($"{DateTime.Now.TimeOfDay}");
         }
     }
 }
+
+
+//var timerState = new TimerState();
+
+//// Delegate that invokes methods for the timer.
+//var timerDelegate = new TimerCallback(CheckStatus);
+
+//// timer that waits one second, then invokes every second.
+//var timer = new Timer(timerDelegate, timerState, 1000, 1000);
+
+//// Keep a handle to the timer, so it can be disposed.
+//timerState.Timer = timer;
+
+//// The main thread does nothing until the timer is disposed.
+//while (timerState.Timer != null)
+//{
+//    Thread.Sleep(0);
+//}
+
+
+
+//private class TimerState
+//{
+//    public int Counter = 0;
+//    public Timer Timer;
+//}
+
+//private static void CheckStatus(object state)
+//{
+//    var mailData = new MailManager();
+//    mailData.MailArrived += (obj, mailevenArgs) => { Console.WriteLine(mailevenArgs.Title); };
+//    mailData.MailArrived += (obj, mailevenArgs) => { Console.WriteLine(mailevenArgs.Body); };
+//    mailData.SimulateMailArrived();
+
+//    var timerState = (TimerState)state;
+//    timerState.Counter++;
+//    Console.WriteLine($"{DateTime.Now.TimeOfDay} Checking Status {timerState.Counter}.");
+
+//}
