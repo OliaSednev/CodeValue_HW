05e956403cec02eb7cddbe1870ff4df05196b7f1
OliaSednev <oliakas999@gmail.com>
2016-07-06 02:45:20 +0300

    .

---
+++ FileFinder/FileFinder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var getAllFiles = new FilesSearching();
+            getAllFiles.GetAllFiles(@"D:\CodeV-College\HW\tests", "Hello.txt");
+            getAllFiles.DisplayFiles();
+
+        }
+
+
+    }
+}
+
