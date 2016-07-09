6fe0dfa90599ebdaad8d7e3272b6e8129b8b710d
OliaSednev <oliakas999@gmail.com>
2016-07-10 01:28:20 +0300

    .

---
+++ EX_Appendix_OliaSednev/Rationals/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Rational rational1 = new Rational(6, 2);
+                Rational rational2 = new Rational(-1, 2);
+                Rational results = new Rational();
+
+                Console.WriteLine("operator +");
+                Console.WriteLine("**************");
+                results = rational1 + rational2;
+                Console.WriteLine(results.ToString());
+
+                Console.WriteLine("\noperator -");
+                Console.WriteLine("**************");
+                results = rational1 - rational2;
+                Console.WriteLine(results.ToString());
+
+                Console.WriteLine("\noperator *");
+                Console.WriteLine("**************");
+                results = rational1 * rational2;
+                Console.WriteLine(results.ToString());
+
+                Console.WriteLine("\noperator /");
+                Console.WriteLine("**************");
+                results = rational1 / rational2;
+                Console.WriteLine(results.ToString());
+
+
+                Console.WriteLine("\nDouble explicit ");
+                Console.WriteLine("**************");
+                Console.WriteLine((double)new Rational(13, 3));
+
+                Console.WriteLine("\nFrom integer ");
+                Console.WriteLine("**************");
+                Console.WriteLine(new Rational(9, 3));
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ EX_Appendix_OliaSednev/Rationals/Rational.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationals
+{
+    struct Rational
+    {
+        public int Numerator { get; private set; }
+        public int Denominator { get; private set; }
+        public double ToDouble { get { return (double)Numerator / Denominator; } }
+
+        //constructors
+        public Rational(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new DivideByZeroException("Divide by zero!!!");
+            }
+            else
+            {
+                Numerator = numerator;
+                Denominator = denominator;
+            }
+        }
+        public Rational(int numerator) : this(numerator, 1) { }
+
+
+        public static Rational operator +(Rational obj1, Rational obj2)
+        {
+            return obj1.Add(obj2);
+        }
+
+        public static Rational operator -(Rational obj1, Rational obj2)
+        {
+            var addMinusToObj2 = new Rational(-obj2.Numerator, obj2.Denominator);
+            return obj1.Add(addMinusToObj2);
+        }
+
+        public static Rational operator *(Rational obj1, Rational obj2)
+        {
+            return obj1.Mul(obj2);
+        }
+
+        public static Rational operator /(Rational obj1, Rational obj2)
+        {
+            var wasReplaced = new Rational(obj2.Denominator, obj2.Numerator);
+            return obj1.Mul(wasReplaced);
+        }
+
+        public static implicit operator Rational(int fromInteger)
+        {
+            return new Rational(fromInteger);
+        }
+
+        public static explicit operator double(Rational obj)
+        {
+            return obj.ToDouble;
+        }
+
+
+        public Rational Add(Rational obj)
+        {
+            if (Denominator == obj.Denominator)
+            {
+                return new Rational(Numerator + obj.Numerator, Denominator);
+            }
+            return new Rational((Numerator * obj.Denominator + obj.Numerator * Denominator),
+                    (Denominator * obj.Denominator));
+        }
+
+        public Rational Mul(Rational obj)
+        {
+            return new Rational(Numerator * obj.Numerator, Denominator * obj.Denominator);
+        }
+
+        public void Reduce()
+        {
+            var greatestCommonDivisor = GreatestCommonDivisor(Numerator, Denominator);
+            Numerator /= greatestCommonDivisor;
+            Denominator /= greatestCommonDivisor;
+        }
+
+        private int GreatestCommonDivisor(int numerator, int denominator)
+        {
+            return (denominator == 0) ? numerator
+                : GreatestCommonDivisor(denominator, numerator % denominator); //recursion
+        }
+
+        public override bool Equals(object obj)
+        {
+            var rational = (Rational)obj;
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            return ((Numerator == rational.Numerator) && (Denominator == rational.Denominator));
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)ToDouble;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("The value is: {0} / {1} = {2}", Numerator, Denominator, ToDouble);
+        }
+    }
+}
