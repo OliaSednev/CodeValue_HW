b89c197eb201c2655ac86e06d7114963218f6638
OliaSednev <oliakas999@gmail.com>
2016-07-16 23:37:55 +0300

    .

--- EX_1_2_OliaSednev/AttribDemo/Program.cs
+++ EX_1_2_OliaSednev/AttribDemo/Program.cs
@@ -11,11 +11,24 @@ namespace AttribDemo
     {
         static void Main(string[] args)
         {
-            Analayze_Assembly _Assembly= new Analayze_Assembly();
-            Console.WriteLine(_Assembly.AnalayzeAssembly(Assembly.GetExecutingAssembly()));
+            try
+            {
+                Analayze_Assembly _Assembly = new Analayze_Assembly();
+                Console.WriteLine(_Assembly.AnalayzeAssembly(Assembly.GetExecutingAssembly()));
 
-            //1.2 - 10) The attributes that I looking could not find in Int Assembly
-            Console.WriteLine(_Assembly.AnalayzeAssembly(typeof(int).Assembly).ToString());
+                //1.2 - 10) The attributes that I looking could not find in Int Assembly
+                Console.WriteLine(_Assembly.AnalayzeAssembly(typeof(int).Assembly).ToString());
+
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (TargetException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
