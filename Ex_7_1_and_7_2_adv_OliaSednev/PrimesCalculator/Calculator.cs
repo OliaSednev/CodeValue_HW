d226d7380dbb30df82e7f6c88bd268ca3379d94d
OliaSednev <oliakas999@gmail.com>
2016-07-30 21:21:35 +0300

    .

---
+++ Ex_7_1_and_7_2_adv_OliaSednev/PrimesCalculator/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrimesCalculator
+{
+    class Calculator
+    {
+
+        public IEnumerable<int> PrimesCalculator(int first, int last, WaitHandle handle)
+        {
+            List<int> listOfPrimesNumbers = new List<int>();
+            for (int i = first; i <= last; i++)
+            {
+                if (handle != null && handle.WaitOne(0))
+                {
+                    break;
+                }
+                if (i == 1) { i++; }
+                if (IsPrime(i))
+                {
+                    listOfPrimesNumbers.Add(i);
+                }
+            }
+            return listOfPrimesNumbers;
+        }
+        private bool IsPrime(int number)
+        {
+            for (int i = 2; i <= (int)Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Ex_7_1_and_7_2_adv_OliaSednev/PrimesCalculator/PrimesCalculatorForm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimesCalculator
+{
+    public partial class PrimesCalculatorForm : Form
+    {
+        private ObservableCollection<int> primesCollection = new ObservableCollection<int>();
+        private Calculator primesCalculator = new Calculator();
+        private CancellationTokenSource cancellationTokenSource;
+        private SynchronizationContext synchronizationContext = SynchronizationContext.Current;
+        
+        public PrimesCalculatorForm()
+        {
+            InitializeComponent();
+        }        
+        
+        private void calculate_button_Click(object sender, EventArgs e)
+        {
+            primesCollection.Clear();
+
+            int first, last=0;
+            cancellationTokenSource = new CancellationTokenSource();
+            var tokenSource = cancellationTokenSource.Token;
+            
+            bool isOk = int.TryParse(first_txtBox.Text, out first) &&
+                int.TryParse(last_textBox.Text, out last);
+            if (!isOk)
+            {
+                MessageBox.Show("Enter numbers only!!!");
+            }
+
+            Task.Run(() =>
+            {
+                var p = primesCalculator.PrimesCalculator(first, last, tokenSource.WaitHandle);
+                primesCollection= new ObservableCollection<int>(p);
+
+                UpdateCollection(primesCollection);
+
+            }, tokenSource);
+        }
+
+
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            if (!cancellationTokenSource.IsCancellationRequested && cancellationTokenSource != null)
+            {
+                var token = cancellationTokenSource.Token;
+                cancellationTokenSource.Cancel();
+
+                token.Register(() =>
+                {
+                    MessageBox.Show("The action was canceled!");
+                });
+            }
+        }
+        private void UpdateCollection(IEnumerable<int> list)
+        {
+            result.Items.Clear();
+            result.BeginUpdate();
+            foreach (int i in list)
+            {
+                result.Items.Add(i);
+            }
+            result.EndUpdate();
+        }
+
+        private void result_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void first_txtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void last_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
