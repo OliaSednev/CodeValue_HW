0d19954d3fea25e7f654c6ddd7bff9fce245d54a
OliaSednev <oliakas999@gmail.com>
2016-08-12 01:53:01 +0300

    .

---
+++ ParallelClass_OliaSednev/Primes/CalcPrimeNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primes
+{
+    class CalcPrimeNumbers
+    {
+        public ICollection<int> CalcPrimes(int first, int last, int degreeOfParallelism)
+        {
+            var list = new List<int>();
+            ParallelOptions parallelOptions = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = degreeOfParallelism
+            };
+
+            Parallel.For(first, last, parallelOptions, i => // lambda expression for parallel loop
+            {
+                var isPrimeNumber = true;
+
+                for (int j = 2; j <= (int)Math.Sqrt(i); j++)
+                    if (i % j == 0)
+                    {
+                        isPrimeNumber = false;
+                        break;
+                    }
+                if (isPrimeNumber && i > 1)
+                {
+                    lock (list)
+                    {
+                        list.Add(i);
+                    }
+                }
+            });
+            return list;
+        }
+    }
+}
--- ParallelClass_OliaSednev/Primes/Program.cs
+++ ParallelClass_OliaSednev/Primes/Program.cs
@@ -12,8 +12,9 @@ namespace Primes
     {
         static void Main(string[] args)
         {
+            CalcPrimeNumbers primeNumbers = new CalcPrimeNumbers();
             var timePerCalcPrimeNumbers = Stopwatch.StartNew();
-            var collectionOfPrimeNumbers = CalcPrimes(0, 11, 2);
+            var collectionOfPrimeNumbers = primeNumbers.CalcPrimes(0, 11, 2);
             timePerCalcPrimeNumbers.Stop();
             Console.WriteLine("The prime numbers:");
             foreach (var item in collectionOfPrimeNumbers)
@@ -23,7 +24,7 @@ namespace Primes
             Console.WriteLine($"\nThe elapsed time: {timePerCalcPrimeNumbers.ElapsedMilliseconds}");
 
             timePerCalcPrimeNumbers.Restart();
-            collectionOfPrimeNumbers = CalcPrimes(0, 21, 3);
+            collectionOfPrimeNumbers = primeNumbers.CalcPrimes(0, 21, 3);
             timePerCalcPrimeNumbers.Stop();
             Console.WriteLine("The prime numbers:");
             foreach (var item in collectionOfPrimeNumbers)
@@ -33,7 +34,7 @@ namespace Primes
             Console.WriteLine($"\nThe elapsed time: {timePerCalcPrimeNumbers.ElapsedMilliseconds}");
 
             timePerCalcPrimeNumbers.Restart();
-            collectionOfPrimeNumbers = CalcPrimes(0, 31, 4);
+            collectionOfPrimeNumbers = primeNumbers.CalcPrimes(0, 31, 4);
             timePerCalcPrimeNumbers.Stop();
             Console.WriteLine("The prime numbers:");
             foreach (var item in collectionOfPrimeNumbers)
@@ -41,35 +42,6 @@ namespace Primes
                 Console.Write($"{item}  ");
             }
             Console.WriteLine($"\nThe elapsed time: {timePerCalcPrimeNumbers.ElapsedMilliseconds}");
-        }
-
-        private static ICollection<int> CalcPrimes(int first, int last, int degreeOfParallelism)
-        {
-            var list = new List<int>();
-            ParallelOptions parallelOptions = new ParallelOptions
-            {
-                MaxDegreeOfParallelism = degreeOfParallelism
-            };
-
-            Parallel.For(first, last, parallelOptions, i => // lambda expression for parallel loop
-            {
-                var isPrimeNumber = true;
-
-                for (int j = 2; j <= (int)Math.Sqrt(i); j++)
-                    if (i % j == 0)
-                    {
-                        isPrimeNumber = false;
-                        break;
-                    }
-                if (isPrimeNumber && i > 1)
-                {
-                    lock (list)
-                    {
-                        list.Add(i);
-                    }
-                }
-            });
-            return list;
         }
     }
 }
