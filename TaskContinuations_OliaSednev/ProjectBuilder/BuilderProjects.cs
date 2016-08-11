49efb82b340d045013b8ad232bc19320c7955557
OliaSednev <oliakas999@gmail.com>
2016-08-12 02:03:46 +0300

    .

--- TaskContinuations_OliaSednev/ProjectBuilder/BuilderProjects.cs
+++ TaskContinuations_OliaSednev/ProjectBuilder/BuilderProjects.cs
@@ -29,15 +29,15 @@ namespace ProjectBuilder
         }
         public void Concurrently()
         {
-            Task task1 = Task.Factory.StartNew(() => Builder(1));
-            Task task2 = Task.Factory.StartNew(() => Builder(2));
-            Task task3 = Task.Factory.StartNew(() => Builder(3));
-            Task task4 = task1.ContinueWith(p => Builder(4));
-            Task task5 = Task.Factory.ContinueWhenAll(new Task[] { task1, task2, task3 }, project => Builder(5));
-            Task task6 = Task.Factory.ContinueWhenAll(new Task[] { task3, task4 }, project => Builder(6));
-            Task task7 = Task.Factory.ContinueWhenAll(new Task[] { task5, task6 }, project => Builder(7));
-            Task task8 = task5.ContinueWith(project => Builder(8));
-            Task.WaitAll(task5, task6, task7, task8);
+            Task task_1 = Task.Factory.StartNew(() => Builder(1));
+            Task task_2 = Task.Factory.StartNew(() => Builder(2));
+            Task task_3 = Task.Factory.StartNew(() => Builder(3));
+            Task task_4 = task_1.ContinueWith(p => Builder(4));
+            Task task_5 = Task.Factory.ContinueWhenAll(new Task[] { task_1, task_2, task_3 }, projects => Builder(5));
+            Task task_6 = Task.Factory.ContinueWhenAll(new Task[] { task_3, task_4 }, projects => Builder(6));
+            Task task_7 = Task.Factory.ContinueWhenAll(new Task[] { task_5, task_6 }, projects => Builder(7));
+            Task task_8 = task_5.ContinueWith(project => Builder(8));
+            Task.WaitAll(task_5, task_6, task_7, task_8);
         }
     }
 }
