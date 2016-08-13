5cb58e73f949870a52d5c48c7917a5d0407f0ec1
OliaSednev <oliakas999@gmail.com>
2016-08-13 20:30:56 +0300

    .

--- Jobs_OliaSednev/Jobs/Program.cs
+++ Jobs_OliaSednev/Jobs/Program.cs
@@ -25,12 +25,12 @@ namespace Jobs
 
             Console.WriteLine("To create new processes, press Enter . . .");
             Console.ReadLine();
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 20; i++)
             {
                 new Job(10240);
             }
 
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 20; i++)
             {
                 new Job(1);
             }
